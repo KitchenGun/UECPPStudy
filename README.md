1af78e66bef8375ff5f28c78cbf62a2ee6494b9e
KitchenGun <kitchengun124@gmail.com>
2021-12-02 14:04:51 +0900

    0일차 파일 체크

---
+++ README.md
@@ -0,0 +1,3 @@
+# UECPPStudy
+SGA CPP 사용해서 공부한 것 결과물
+Developed with Unreal Engine 4
---
+++ Source/UECPPStudy/UECPPStudy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UECPPStudy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UECPPStudy, "UECPPStudy" );
